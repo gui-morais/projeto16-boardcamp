cb16a4c3aa84d4ebac0117ebda81b1144700eaeb
Guilherme Morais <guilherme.morais0111@gmail.com>
2022-12-11 17:47:22 -0300

    feat: update client

---
+++ src/middlewares/validateUpdateClient.js
@@ -0,0 +1,31 @@
+import connection from "../database/db.js";
+import { newClientSchema } from "../models/models.js";
+
+export async function validateUpdateClient(req, res, next) {
+    const validation = newClientSchema.validate(req.body, { abortEarly: false });
+    if(validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(400).send(errors);
+    }
+
+    try{
+        const newClient = req.body;
+        const id_params = Number(req.params.id);
+        const checkID = await connection.query("SELECT * FROM customers WHERE id = $1", [id_params]);
+        if(checkID.rows.length===0) {
+            return res.sendStatus(404);
+        }
+        const checkClient = await connection.query("SELECT * FROM customers WHERE cpf = $1", [newClient.cpf]);
+        if(checkClient.rows.length!==0) {
+            const id_db = checkClient.rows[0].id;
+            if(id_db!==id_params) {
+                return res.sendStatus(409)
+            }
+        }
+
+        next();
+    } catch(err) {
+        console.log(err);
+        return res.sendStatus(500);
+    }
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,11 +4,13 @@ import { postCategory } from "../controllers/categories/postCategory.js";
 import { getClient } from "../controllers/clients/getClient.js";
 import { getClients } from "../controllers/clients/getClients.js";
 import { postClient } from "../controllers/clients/postClient.js";
+import { putClient } from "../controllers/clients/putClient.js";
 import { getGames } from "../controllers/games/getGames.js";
 import { postGame } from "../controllers/games/postGame.js";
 import { validateNewCategory } from "../middlewares/validateNewCategory.js";
 import { validateNewClient } from "../middlewares/validateNewClient.js";
 import { validateNewGame } from "../middlewares/validateNewGame.js";
+import { validateUpdateClient } from "../middlewares/validateUpdateClient.js";
 
 const router = express.Router();
 router.post("/categories", validateNewCategory, postCategory);
@@ -18,5 +20,6 @@ router.get("/games", getGames);
 router.post("/customers", validateNewClient, postClient);
 router.get("/customers", getClients);
 router.get("/customers/:id", getClient);
+router.put("/customers/:id", validateUpdateClient, putClient);
 
 export default router;

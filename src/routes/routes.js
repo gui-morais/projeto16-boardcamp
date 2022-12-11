a1130f990eda495f20078ad9766e6b7007d2db42
Guilherme Morais <guilherme.morais0111@gmail.com>
2022-12-11 15:20:04 -0300

    feat: get games

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { getCategories } from "../controllers/categories/getCategories.js";
 import { postCategory } from "../controllers/categories/postCategory.js";
+import { getGames } from "../controllers/games/getGames.js";
 import { postGame } from "../controllers/games/postGame.js";
 import { validateNewCategory } from "../middlewares/validateNewCategory.js";
 import { validateNewGame } from "../middlewares/validateNewGame.js";
@@ -9,5 +10,6 @@ const router = express.Router();
 router.post("/categories", validateNewCategory, postCategory);
 router.get("/categories", getCategories);
 router.post("/games", validateNewGame, postGame);
+router.get("/games", getGames);
 
 export default router;
